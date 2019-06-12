be4f821985a42ec958761fcfe874c3ea2e6deac6
sai <krishmuthavarapu@gmail.com>
2019-06-13 00:58:21 +0530

    part

--- modal.php
+++ modal.php
@@ -1,3 +1,4 @@
+<!-- we think -->
 <div class="modal fade" id="think1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -32,7 +33,7 @@
             </div>
             <div class="modal-body text-center">
                 <div class="future-img">
-                    <img src="img/feature/mach.png" class="img-fluid mb-4" alt="">
+                    <img src="img/feature/bp.png" class="img-fluid mb-4" alt="">
                 </div>
                 <p class="mb-0">We implement the pipeline fitting into common frame work in an integrated fashion. We help our customers to build the pipeline with minimal data move operations and computationally efficiency.</p>
 
@@ -77,7 +78,7 @@
             </div>
             <div class="modal-body text-center">
                 <div class="future-img">
-                    <img src="img/feature/ad.png" class="img-fluid mb-4" alt="">
+                    <img src="img/feature/turn.png" class="img-fluid mb-4" alt="">
                 </div>
                 <p class="mb-0">We encourage our clients to come up with their needs. We take up the responsibility in building the pipe line and scoping out the effort and realization possibilities. This kind of description and the purpose described is enough for our teams to enhance the idea considering the feasibility study.</p>
 
@@ -99,7 +100,7 @@
             </div>
             <div class="modal-body text-center">
                 <div class="future-img">
-                    <img src="img/feature/mach.png" class="img-fluid mb-4" alt="">
+                    <img src="img/feature/pip.png" class="img-fluid mb-4" alt="">
                 </div>
                 <p class="mb-0">The pipeline architecture is about proposing a pipeline of the processing to make the idea realized. The pipe line Architecture involves the following phases.
                     Input read &#8594;Feature computation &#8594;detect the object &#8594;Classify the object &#8594;update the count.
@@ -123,9 +124,145 @@
             </div>
             <div class="modal-body text-center">
                 <div class="future-img">
-                    <img src="img/feature/turn.png" class="img-fluid mb-4" alt="">
+                    <img src="img/feature/mach.png" class="img-fluid mb-4" alt="">
                 </div>
                 <p class="mb-0">Most of the computationally intensive tasks are required to be ported on to some kind of parallel architecture. We do port pipeline built over any hardware platforms based on the need.
+                </p>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- we do -->
+<div class="modal fade" id="wedo1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header ">
+                <h5 class="modal-title" id="exampleModalLabel">Natural language processing (NLP)</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-center">
+                <div class="future-img">
+                    <img src="img/feature/ad.png" class="img-fluid mb-4" alt="">
+                </div>
+                <p class="mb-0">We help our customers in bridging the gap between human communication and digital data, and engage their customers in more meaningful ways.  We offer language processing services, by blending linguistic knowledge and AI skills. Fully extensible knowledge graph and custom integration needs </p>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="wedo2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header ">
+                <h5 class="modal-title" id="exampleModalLabel">Data Analytics</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-center">
+                <div class="future-img">
+                    <img src="img/feature/bp.png" class="img-fluid mb-4" alt="">
+                </div>
+                <p class="mb-0">Neuronoids turns technology into business outcomes by delivering Information management, business intelligence and analytic solutions under one umbrella.  We collaborate with our clients in building their proprietary solution for their hidden data.</p>
+
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="wedo3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header ">
+                <h5 class="modal-title" id="exampleModalLabel">Computer Vision</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-center">
+                <div class="future-img">
+                    <img src="img/feature/dev.png" class="img-fluid mb-4" alt="">
+                </div>
+                <p class="mb-0">Computer Vision (CV) is a field devoted for analyzing, modifying, and detailed level understanding of images.  We enable the companies with the vision technology reducing their time to market with our expertise in realizing the algorithms.</p>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="wedo4" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header ">
+                <h5 class="modal-title" id="exampleModalLabel">Machine Learning</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-center">
+                <div class="future-img">
+                    <img src="img/feature/turn.png" class="img-fluid mb-4" alt="">
+                </div>
+                <p class="mb-0">Machine Learning is a part of Artificial Intelligence. It builds algorithms that allow computers to learn how to perform tasks from data instead of requiring a programmer to write code for those tasks. Machine Learning does this by generalizing from examples and it is a more cost-effective and faster approach when Big Data is involved.</p>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="wedo5" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header ">
+                <h5 class="modal-title" id="exampleModalLabel">Deep Learning</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-center">
+                <div class="future-img">
+                    <img src="img/feature/pip.png" class="img-fluid mb-4" alt="">
+                </div>
+                <p class="mb-0">To maximize your data science potential, it is necessary to determine whether your data science capabilities are appropriate to make a significant business impact. We do offer the Annotation services, Building Deep Learning models, Compression of the complex models while working closely with our clients.
+                </p>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="wedo6" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header ">
+                <h5 class="modal-title" id="exampleModalLabel">Turnkey Solutions</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-center">
+                <div class="future-img">
+                    <img src="img/feature/turn.png" class="img-fluid mb-4" alt="">
+                </div>
+                <p class="mb-0">Turnkey is a product or service that is designed, supplied, built, or installed fully complete and ready to operate.
                 </p>
 
             </div>
